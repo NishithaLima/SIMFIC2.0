73450ef7ac58d7e423d9e12dea0b30961ae2592d
NishithaLima <nancynishitha@gmail.com>
2020-05-22 13:52:29 +0200

    updates to deploy to heroku issue - adding console messages

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + './dist/fiction-search-demo'));
+app.use(express.static(__dirname + './dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'./dist/fiction-search-demo/index.html'));
+res.sendFile(path.join(__dirname+'./dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+Console.log('Console Listening');
