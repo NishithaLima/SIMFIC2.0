33cad8b79567d9636df04449f9a99d0c887b5910
Nishitha Nancy Lima <n.lima@3dqr.de>
2020-11-09 12:32:16 +0100

    Bug fix in saving data

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/fiction-search/'));
+app.use(express.static(__dirname + '/dist/fiction-ui-demo/'));
 
 app.get('*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/fiction-search/index.html'));
+res.sendFile(path.join(__dirname+'/dist/fiction-ui-demo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
